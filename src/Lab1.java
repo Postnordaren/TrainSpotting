2a70305637db5d841e1a4f0b93695843a71781a8
postnordaren <issaemma@live.se>
2025-09-06 00:25:59 +0200

    hej

--- src/Lab1.java
+++ src/Lab1.java
@@ -6,11 +6,18 @@ public class Lab1 {
   private static final int MAX_SPEED = 20; //vi kör detta än sålänge..
   public int sleeptime;
 
-  public static Semaphore northStationSemaphore = new Semaphore(1); 
-  public static Semaphore southStationSemaphore = new Semaphore(1); 
+  // Semaphores for our station
+  public static Semaphore northStation1Semaphore = new Semaphore(1);
+  public static Semaphore northStation2Semaphore = new Semaphore(1); 
+  public static Semaphore southStation1Semaphore = new Semaphore(1); 
+  public static Semaphore southStation2Semaphore = new Semaphore(1);
 
-  public static Semaphore crossSemaphore = new Semaphore(1);
-
+  // Semaphores for other critical points
+  public static Semaphore track1 = new Semaphore(1);
+  public static Semaphore track2 = new Semaphore(1);
+  public static Semaphore track3 = new Semaphore(1);
+  public static Semaphore track4 = new Semaphore(1);
+  
 
   public Lab1(int speed1, int speed2) {
 
@@ -18,20 +25,10 @@ public class Lab1 {
 
     Thread train1 = new Thread(new Train(1, speed1));
     Thread train2 = new Thread(new Train(2, speed2));
-
-    try {
-      //tsi.setSwitch(17, 7, TSimInterface.SWITCH_RIGHT);
-      //tsi.setSwitch(3, 11, TSimInterface.SWITCH_RIGHT);
-     // tsi.setSwitch(4, 9, TSimInterface.SWITCH_RIGHT);
-
-    }
-    catch (Exception e) {
-
-    }
     train1.start();
     train2.start();
-  }
 
+  }
 
   public class Train implements Runnable {
     int trainId;
@@ -78,22 +75,6 @@ public class Lab1 {
       tsi.setSpeed(trainId, this.speed);
     } 
 
-    // Metod för att byta switch
-
-    // sensor 15,7 : switch 17,7 till upp
-    // sensor 18,7 : switch 17,7 till ner
-
-    // sensor 16,9 : switch 15,9 till ner
-    // sensor 14,9 : switch 15,9 till upp
-
-    // sensor 6,10 : switch 4,9 till ner
-    // sensor 2,9 : switch 4,9 till upp
-
-    // sensor 2,11 : switch 3,11 till ner
-    // sensor 3,12 : switch 3,11 till upp
-
-    // sensor 4,11 : switch 3,11 till upp
-
     public void handleSwitch(SensorEvent sensor) throws CommandException {
 
       int x = sensor.getXpos();
@@ -125,7 +106,7 @@ public class Lab1 {
     //if (sensor.getStatus() == SensorEvent.ACTIVE) { // Bara när tåget kommer in
         try {
             System.out.println(sensor.getXpos() + " " + sensor.getYpos() + " ...");
-            Lab1.northStationSemaphore.acquire();
+            Lab1.northStation1Semaphore.acquire();
             tsi.setSpeed(trainId, 0);
             Thread.sleep(sleepTime);
             switchDirection();
@@ -133,16 +114,15 @@ public class Lab1 {
             e.printStackTrace();
 
         } finally {
-            Lab1.northStationSemaphore.release();
+            Lab1.northStation1Semaphore.release();
         }
-    //}
   }
 
   public void southStationSensor(SensorEvent sensor) {
     //if (sensor.getStatus() == SensorEvent.ACTIVE) { // Bara när tåget kommer in
         try {
             System.out.println(sensor.getXpos() + " " + sensor.getYpos() + " ...");
-            Lab1.northStationSemaphore.acquire();
+            Lab1.southStation1Semaphore.acquire();
             tsi.setSpeed(trainId, 0);
             Thread.sleep(sleepTime);
             switchDirection();
@@ -150,21 +130,10 @@ public class Lab1 {
             e.printStackTrace();
 
         } finally {
-            Lab1.southStationSemaphore.release();
+            Lab1.southStation1Semaphore.release();
         }
-    //}
-  }
-
-  public void crossSensor() throws CommandException {  
-      try {
-        Lab1.crossSemaphore.acquire();
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      } finally {
-        Lab1.crossSemaphore.release();
       }
-  }
+    }
 
     private boolean isNorthStation(int x, int y) {
       return (x == 14 && y == 5) || (x == 16 && y == 11);
@@ -173,17 +142,8 @@ public class Lab1 {
     private boolean isSouthStation(int x, int y) {
       return (x == 14 && y == 13) || (x == 14 && y == 4);
     }
-
-
-    private boolean isSensorbyCross(int x, int y) {
-      if (x == 7 && y == 7 || x == 8 && y == 8 || x == 15 && y == 7 || x == 18 && y == 7 || x == 16 && y == 9 || x == 14 && y == 9 ||
-      x == 2 && y == 9 || x == 6 && y == 10 || x == 2 && y == 11 || x == 4 && y == 11 || x == 3 && y == 12 || x == 8 && y == 6){
-        return true;
-      }
-      return false;
-    }
   }
-}
+
 
 
 
